70ee49671eec2413cd1dc9812d8bd535dec89f71
kamilp <Kamil Piekutowski>
2020-09-14 01:51:36 -0500

    added main.c a hello world module

---
+++ 001hello_world/main.c
@@ -0,0 +1,20 @@
+#include <linux/module.h>
+
+static int __init helloworld_init(void)
+{
+  pr_info("Hello world\n");
+  return 0;
+}
+
+static void __exit helloworld_cleanup(void)
+{
+  pf_info("Goddbye world!\n");
+}
+
+module_init(helloworld_init);
+module_exit(helloworld_cleanup);
+
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("Kamil Piekutowski");
+MODULE_DESCRIPTION("A simple hello world");
+MODULE_INFO(board, "Beaglebone Black REv C");
